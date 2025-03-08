1dfc045d7ca5899e6626188d626dc5a50713bf9d
CodeWithMian <mb03086350364@gmail.com>
2025-03-08 15:51:06 +0500

    Change-[Loader]

--- src/components/loader/Loader.tsx
+++ src/components/loader/Loader.tsx
@@ -2,16 +2,11 @@ import React from "react";
 
 export default function Loader() {
   return (
-    <div className="flex justify-center items-center h-screen">
-      <div className="w-24 h-24 relative">
-        <div className="absolute inset-0 bg-gradient-to-r from-blue-400 to-blue-600 rounded-full animate-pulse"></div>
-        <div className="absolute inset-2 bg-gradient-to-r from-blue-600 to-blue-800 rounded-full animate-pulse"></div>
-        <div className="absolute inset-4 bg-gradient-to-r from-blue-800 to-blue-900 rounded-full animate-pulse"></div>
-        <div className="absolute inset-6 bg-gradient-to-r from-blue-900 to-blue-800 rounded-full animate-pulse"></div>
-        <div className="absolute inset-8 bg-gradient-to-r from-blue-800 to-blue-600 rounded-full animate-pulse"></div>
-        <div className="absolute inset-10 bg-gradient-to-r from-blue-600 to-blue-400 rounded-full animate-pulse"></div>
-        <div className="absolute inset-12 bg-gradient-to-r from-blue-400 to-blue-200 rounded-full animate-pulse"></div>
-        <div className="absolute inset-14 bg-gradient-to-r from-blue-200 to-blue-100 rounded-full animate-pulse"></div>
+    <div className="flex justify-center items-center h-screen bg-gray-900">
+      <div className="relative w-24 h-24 flex items-center justify-center">
+        <div className="absolute w-full h-full animate-spin rounded-full border-4 border-transparent border-t-blue-500 border-r-blue-500 shadow-lg"></div>
+        <div className="absolute w-16 h-16 animate-spin-slow rounded-full border-4 border-transparent border-b-blue-400 border-l-blue-400 shadow-md"></div>
+        <div className="w-10 h-10 bg-blue-500 rounded-full animate-pulse shadow-2xl"></div>
       </div>
     </div>
   );
--- src/instance/instance.ts
+++ src/instance/instance.ts
@@ -5,3 +5,19 @@ const axiosInstance = axios.create({
 });
 
 export default axiosInstance;
+
+
+// export const fetchMovies = createAsyncThunk(
+//   "movies/fetchMovies",
+//   async (_, { rejectWithValue }) => {
+//     try {
+//       const response = await axios.get(
+//         `https://api.themoviedb.org/3/movie/popular?api_key=${import.meta.env.VITE_API_KEY}`
+//       );
+//       return response.data.results; 
+//     } catch (error) {
+      
+//       return rejectWithValue(error.response ? error.response.data : error.message);
+//     }
+//   }
+// );
